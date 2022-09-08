c76724493435522c9993b6338355dbe070a087de
simjeongsoo <simking95@naver.com>
2022-09-08 14:59:54 +0900

    Application.java 주석추가

--- api/basicapi01/src/main/java/com/springboot/myreference/basicapi/Application.java
+++ api/basicapi01/src/main/java/com/springboot/myreference/basicapi/Application.java
@@ -3,9 +3,12 @@ package com.springboot.myreference.basicapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 프로젝트의 main class
+// @SpringBootApplication 를 통해 스프링부트 자동생성 , Bean 읽기 생성 자동설정
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        // SpringApplication.run 으로 내장 WAS 실행
         SpringApplication.run(Application.class, args);
     }
 }
