00474f151e4547d6d8da65d0284d2ada8dd978d0
simjeongsoo <simking95@naver.com>
2022-09-16 15:19:49 +0900

    템플릿 엔진에 파라미터 전달

--- inflearn/spring-introduction/hello-spring /src/main/java/group/hellospring/controller/HelloController.java
+++ inflearn/spring-introduction/hello-spring /src/main/java/group/hellospring/controller/HelloController.java
@@ -3,13 +3,19 @@ package group.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-
     @GetMapping("hello")
-    public String hello(Model model) {
+    public String hello(Model model) { // mvc 예시
         model.addAttribute("data", "Spring");
         return "hello"; // resources:templates/ + {ViewName} + .html
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name",required = false) String name, Model model) { // 외부에서 파라미터를 받는 방식
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
