dc06d14f8256e071c544d2a347599430e5cc9178
simjeongsoo <simking95@naver.com>
2022-09-20 18:14:14 +0900

    컴포넌트 스캔과 자동 의존관계 설정

---
+++ inflearn/spring-introduction/member-management/src/main/java/com/sim/membermanagement/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.sim.membermanagement.controller;
+
+import com.sim.membermanagement.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller //어노테이션을 달면 아무런기능이 없는 MemberController도 스프링컨테이너가 객체를 만들어 bean에 넣어두고 관리
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired // 의존관계 추가
+    public MemberController(MemberService memberService) { // MemberService 주입 
+        this.memberService = memberService;
+    }
+}
