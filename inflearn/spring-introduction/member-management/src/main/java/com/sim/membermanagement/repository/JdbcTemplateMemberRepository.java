599dbf8e1e6bd6074ac695cff68e31340af78fcf
simjeongsoo <simking95@naver.com>
2022-09-21 17:59:19 +0900

    create 스프링 JdbcTemplate 회원 리포지토리

---
+++ inflearn/spring-introduction/member-management/src/main/java/com/sim/membermanagement/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,75 @@
+package com.sim.membermanagement.repository;
+
+import com.sim.membermanagement.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired // 스프링 빈으로 등록되었을 때 생성자가 하나면 생략 가능
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        // insert 쿼리를 자동으로 만들어줌
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        member.setId(key.longValue());
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) { // 조회쿼리
+        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(), id);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) { // 조회쿼리
+        List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from momber", memberRowMapper());
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return (RowMapper<Member>) (rs, rowNum) -> { // resultSet 결과를 받아서
+            Member member = new Member(); // member로 객체를 매핑
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member; // 리턴
+        };
+        /*
+        * return new RowMapper<Member>() {
+            @Override
+            public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return member;
+            }
+        }*/
+    }
+
+}
