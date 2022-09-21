376b016813561649ef859c9d302ac472fec361c9
simjeongsoo <simking95@naver.com>
2022-09-21 16:21:54 +0900

    add SpringConfig 변경, jdbc로 구현체 변경

--- inflearn/spring-introduction/member-management/src/main/java/com/sim/membermanagement/SpringConfig.java
+++ inflearn/spring-introduction/member-management/src/main/java/com/sim/membermanagement/SpringConfig.java
@@ -1,13 +1,23 @@
 package com.sim.membermanagement;
 
+import com.sim.membermanagement.repository.JdbcMemberRepository;
 import com.sim.membermanagement.repository.MemberRepository;
-import com.sim.membermanagement.repository.MemoryMemberRepository;
 import com.sim.membermanagement.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource; // spring boot가 application.property를 스캔해 자체적으로 DB와 연결하는 DataSource 빈 생성
+
+    @Autowired // DI
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean // bean 등록 어노테이션
     public MemberService memberService() {
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);// 구현체 변경, OCP
     }
 }
