0a491aba26aacb89406f0097bb04344cc45baa6d
simjeongsoo <simking95@naver.com>
2022-09-21 19:02:11 +0900

    JPA 엔티티 매핑

--- inflearn/spring-introduction/member-management/src/main/java/com/sim/membermanagement/domain/Member.java
+++ inflearn/spring-introduction/member-management/src/main/java/com/sim/membermanagement/domain/Member.java
@@ -1,9 +1,15 @@
 package com.sim.membermanagement.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
     // 비즈니스 요구사항에서 필요한 데이터 2 가지 필드값
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 시스템이 구분하기 위한 id 값(sequence 값이 넘어옴)
     private String name; // 고객이 회원가입할 때 적는 이름값
+
 
     public Long getId() {
         return id;
