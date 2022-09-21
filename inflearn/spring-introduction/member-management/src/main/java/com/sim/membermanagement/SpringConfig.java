fbd3ac8d2a3320f67205e6d0c09cef673cc32dab
simjeongsoo <simking95@naver.com>
2022-09-21 19:08:25 +0900

    JPA 를 사용하도록 스프링 설정 변경

--- inflearn/spring-introduction/member-management/src/main/java/com/sim/membermanagement/SpringConfig.java
+++ inflearn/spring-introduction/member-management/src/main/java/com/sim/membermanagement/SpringConfig.java
@@ -1,21 +1,25 @@
 package com.sim.membermanagement;
 
 import com.sim.membermanagement.repository.JdbcTemplateMemberRepository;
+import com.sim.membermanagement.repository.JpaMemberRepository;
 import com.sim.membermanagement.repository.MemberRepository;
 import com.sim.membermanagement.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource; // spring boot가 application.property를 스캔해 자체적으로 DB와 연결하는 DataSource 빈 생성
+    private final EntityManager em; // 엔티티를 관리하는 역할
+    private final DataSource dataSource; // spring boot가 application.property를 스캔해 자체적으로 DB와 연결하는 DataSource 빈 생성
 
-    @Autowired // DI
-    public SpringConfig(DataSource dataSource) {
+    @Autowired
+    public SpringConfig(EntityManager em, DataSource dataSource) {
+        this.em = em;
         this.dataSource = dataSource;
     }
 
@@ -28,6 +32,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource); // 구현체 교체, OCP
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em); // 구현체 교체, OCP
     }
 }
