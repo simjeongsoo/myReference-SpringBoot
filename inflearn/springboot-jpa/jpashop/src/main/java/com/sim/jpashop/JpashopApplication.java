76f1e02953051af65a0ca748cdd06906e9226f48
simjeongsoo <simking95@naver.com>
2022-09-22 15:54:44 +0900

    Lombok 적용 테스트

--- inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/JpashopApplication.java
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/JpashopApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpashopApplication {
-
 	public static void main(String[] args) {
+
+		LombokT lombokT = new LombokT();
+		lombokT.setData("lombok test");
+		String data = lombokT.getData();
+		System.out.println("data = " + data);
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
---
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/LombokT.java
@@ -0,0 +1,9 @@
+package com.sim.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LombokT {
+    private String data;
+}
