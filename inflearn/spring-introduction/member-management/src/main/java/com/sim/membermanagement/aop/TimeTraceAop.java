0c44f819d4c5d7e6d83132609d0f99937641ceb8
simjeongsoo <simking95@naver.com>
2022-09-22 14:09:41 +0900

    AOP 생성

---
+++ inflearn/spring-introduction/member-management/src/main/java/com/sim/membermanagement/aop/TimeTraceAop.java
@@ -0,0 +1,27 @@
+package com.sim.membermanagement.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component // SpringConfig 에서 bean 등록하는게 좋음
+public class TimeTraceAop {
+    // 메서드 수행 시간 측정 AOP
+
+    @Around("execution(* com.sim.membermanagement..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+//            Object result = joinPoint.proceed();
+//            return result;
+            return joinPoint.proceed();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
