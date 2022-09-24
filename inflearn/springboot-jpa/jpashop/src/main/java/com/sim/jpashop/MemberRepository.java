8b38ccf5e07633b227097b49af0911bf95d14d13
simjeongsoo <simking95@naver.com>
2022-09-24 10:47:11 +0900

    create MemberRepository

---
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/MemberRepository.java
@@ -0,0 +1,27 @@
+package com.sim.jpashop;
+// 엔티티 같은것들을 찾아주는 역할
+// DAO 와 유사
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+
+
+}
