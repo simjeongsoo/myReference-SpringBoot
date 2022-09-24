569be5ef888240735950fef8e40f5e7b87887acc
simjeongsoo <simking95@naver.com>
2022-09-24 10:47:46 +0900

    create MemberRepositoryTest

---
+++ inflearn/springboot-jpa/jpashop/src/test/java/com/sim/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.sim.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void save() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member = " + (findMember == member));
+
+    }
+    @Test
+    public void find() throws Exception{
+        //given
+
+        //when
+
+        //then
+    }
+}
