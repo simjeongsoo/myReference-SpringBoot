d3147ddd99455cba46933f537f3573d86f78c988
simjeongsoo <simking95@naver.com>
2022-10-18 19:45:56 +0900

    [Spring Security 설정] - h2-console 접근을 위한 설정 추가

--- jwt/src/main/java/com/sim/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/sim/jwt/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.sim.jwt.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity  // Spring Security 설정 활성화
@@ -18,5 +19,16 @@ public class SecurityConfig {
                 .anyRequest().authenticated();
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        //--h2-console 하위의 모든 요청들과 파비콘 관련 요청은 Spring Security 로직을 수행하지 않고 접근설정--//
+        return (web -> web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        , "/favicon.ico")
+        );
     }
 }
