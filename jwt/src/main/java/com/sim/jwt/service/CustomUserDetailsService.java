c0012aa78422792882558bcb421173f200700df9
simjeongsoo <simking95@naver.com>
2022-10-19 20:52:14 +0900

    [sevice] - Spring Security에서 중요한 부분중 하나인 UserDetailsService를 구현한 클래스

--- jwt/src/main/java/com/sim/jwt/service/CustomUserDetailsService.java
+++ jwt/src/main/java/com/sim/jwt/service/CustomUserDetailsService.java
@@ -6,17 +6,18 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component("userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
-    //--Spring Security에서 중요한 부분중 하나인 UserDetailsService를 구현한 클래스
-    // UserDetailsService를 implements--//
+    //--Spring Security에서 중요한 부분중 하나인 UserDetailsService를 구현한 클래스--//
 
     private final UserRepository userRepository;
-    // UserRepository 주입
+
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -24,25 +25,18 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) {
-        //--loadUserByUsername() 메서드 오버라이드--//
-
-        // 로그인시에 DB에서 유저정보와 권한정보를 가져옴
         return userRepository.findOneWithAuthoritiesByUsername(username)
                 .map(user -> createUser(username, user))
                 .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
     }
 
     private org.springframework.security.core.userdetails.User createUser(String username, com.sim.jwt.entity.User user) {
-
-        // 유저가 활성화 상태라면
         if (!user.isActivated()) {
             throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
         }
         List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
                 .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
                 .collect(Collectors.toList());
-
-        // 로그인시에 가져온 DB에서 유저정보와 권한정보를 기반으로 userdetails.User 객체 생성후 리턴
         return new org.springframework.security.core.userdetails.User(user.getUsername(),
                 user.getPassword(),
                 grantedAuthorities);
