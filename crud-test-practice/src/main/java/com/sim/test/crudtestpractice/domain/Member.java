31a7cc141528f64e1a6d349c97527fd9110d61eb
simjeongsoo <simking95@naver.com>
2022-10-22 01:08:15 +0900

    [도메인] - 도메인 구현

---
+++ crud-test-practice/src/main/java/com/sim/test/crudtestpractice/domain/Member.java
@@ -0,0 +1,30 @@
+package com.sim.test.crudtestpractice.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @Builder
+    public Member(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
