b64f27ed36a4ce24cdefe474d53ff62170799902
simjeongsoo <simking95@naver.com>
2022-10-22 01:03:54 +0900

    init project

---
+++ crud-test-practice/src/main/java/com/sim/test/crudtestpractice/CrudTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sim.test.crudtestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTestPracticeApplication.class, args);
+    }
+
+}
---
+++ crud-test-practice/src/test/java/com/sim/test/crudtestpractice/CrudTestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sim.test.crudtestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTestPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
