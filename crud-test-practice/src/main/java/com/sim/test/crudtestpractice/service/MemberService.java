208419befd1dcde3ca9dd26640d5eb4defc27388
simjeongsoo <simking95@naver.com>
2022-11-01 18:36:07 +0900

    [dto] - 파라미터를 dto로 받도록 수정

--- crud-test-practice/src/main/java/com/sim/test/crudtestpractice/service/MemberService.java
+++ crud-test-practice/src/main/java/com/sim/test/crudtestpractice/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.sim.test.crudtestpractice.service;
 
 import com.sim.test.crudtestpractice.domain.Member;
+import com.sim.test.crudtestpractice.dto.MemberJoinRequestDto;
 import com.sim.test.crudtestpractice.repository.MemberRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,15 +22,26 @@ public class MemberService {
     /**
      * 회원가입 로직 구현
      */
-    public Long join(Member member) {
-        validateDuplicateMember(member);        // 중복 회원 조회 로직
-        memberRepository.save(member);          // 회원 정보 DB 저장 로직
-        return member.getId();                  // 회원 id 값 반환
+//    public Long join(Member member) {
+//        validateDuplicateMember(member);        // 중복 회원 조회 로직
+//        memberRepository.save(member);          // 회원 정보 DB 저장 로직
+//        return member.getId();                  // 회원 id 값 반환
+//    }
+//
+//    private void validateDuplicateMember(Member member) {
+//        //--중복 회원 조회--//
+//        memberRepository.findByName(member.getName()).ifPresent(i -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+//    }
+    public Long join(MemberJoinRequestDto joinRequestDto) {
+        // Controller 계층에서 dto로 데이터를 받아 Service 계층에서 entity로 변환
+        validateDuplicateMember(joinRequestDto);
+        return memberRepository.save(joinRequestDto.toEntity()).getId();
     }
 
-    private void validateDuplicateMember(Member member) {
-        //--중복 회원 조회--//
-        memberRepository.findByName(member.getName()).ifPresent(i -> {
+    private void validateDuplicateMember(MemberJoinRequestDto joinRequestDto) {
+        memberRepository.findByName(joinRequestDto.toEntity().getName()).ifPresent(i->{
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
     }
