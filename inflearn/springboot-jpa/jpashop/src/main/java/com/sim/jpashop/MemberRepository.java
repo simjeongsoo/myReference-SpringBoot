cbbf7259851f8fb46e228215eb3fb3e2f862b8fb
simjeongsoo <simking95@naver.com>
2022-11-03 17:08:54 +0900

    JPA와 DB설정, 동작확인

--- inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/MemberRepository.java
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/MemberRepository.java
@@ -10,10 +10,13 @@ import javax.persistence.PersistenceContext;
 @Repository
 public class MemberRepository {
 
-    @PersistenceContext
+    @PersistenceContext // springboot 자동 주입
     private EntityManager em;
 
     public Long save(Member member) {
+        // return 값을 Member객체가 아닌 Long으로 받는 이유
+        // commend와 쿼리를 분리
+        // save를 동작하고 나면 사이드이펙트를 일으키는 커멘드 성이기 때문에 리턴값을 만들지 않음
         em.persist(member);
         return member.getId();
     }
@@ -21,7 +24,4 @@ public class MemberRepository {
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
-
-
-
 }
--- inflearn/springboot-jpa/jpashop/src/test/java/com/sim/jpashop/MemberRepositoryTest.java
+++ inflearn/springboot-jpa/jpashop/src/test/java/com/sim/jpashop/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+
+// spring 종속적으로 설계 하기 때문에 springframework 의 transaction 사용
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
@@ -17,9 +19,9 @@ class MemberRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
-    @Transactional
+    @Transactional  // entity manager를 통한 모든 데이터 변경은 항상 transaction 안에서 이루어 져야 함
     @Rollback(value = false)
-    public void save() throws Exception{
+    public void memberTest() throws Exception{
         //given
         Member member = new Member();
         member.setUsername("memberA");
@@ -31,16 +33,11 @@ class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        // true , 같은 트랜잭션 안에서 저장하고 조회하면 같은 영속성 컨텍스트가 같음 , id 값이 같으면 같은 entity로 인식
+        // JPA 엔티티 동일성 보장
         Assertions.assertThat(findMember).isEqualTo(member);
         System.out.println("findMember == member = " + (findMember == member));
 
-    }
-    @Test
-    public void find() throws Exception{
-        //given
-
-        //when
-
-        //then
     }
 }
