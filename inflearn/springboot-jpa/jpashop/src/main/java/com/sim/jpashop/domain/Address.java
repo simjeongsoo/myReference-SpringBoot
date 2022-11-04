9d14e00395f53787dce257aead8f1d8fa0e7789f
simjeongsoo <simking95@naver.com>
2022-11-04 18:23:00 +0900

    [Entity 개발] - Address 값 타입을 변경 불가능하게 설계

--- inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/domain/Address.java
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/domain/Address.java
@@ -13,4 +13,14 @@ public class Address {
     private String street;
     private String zipcode;
 
+    // JPA 스펙상 엔티티나 임베디드 타입( @Embeddable )은 자바 기본 생성자(default constructor)를 public 또는protected 로 설정해야 한다.
+    protected Address() {
+    }
+
+    // 값 타입은 변경 불가능하게 설계해야 함, 생성자에서 값을 모두 초기화해서 변경 불가능한 클래스로 만듬
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
