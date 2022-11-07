babb1e31c9ebefd17345b7c3daa359ab182eaeca
simjeongsoo <simking95@naver.com>
2022-11-07 17:39:58 +0900

    [상품 도메인 개발] - 재고수량 비즈니스 로직 추가

--- inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/domain/item/Item.java
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.sim.jpashop.domain.item;
 
 import com.sim.jpashop.domain.Category;
+import com.sim.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -32,4 +33,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //--비즈니스 로직--//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity; // 남은 재고 = 현재 재고 - 주문수량
+        if (restStock < 0) { // 남은 재고가 0보다 작다면
+            throw new NotEnoughStockException("need more stock"); // EXCEPTION
+        }
+        this.stockQuantity = restStock;
+    }
 }
