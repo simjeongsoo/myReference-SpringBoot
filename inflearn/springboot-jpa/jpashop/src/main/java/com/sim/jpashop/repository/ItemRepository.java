540c54f72cc6f7df6629cde23f047920bb3e86b8
simjeongsoo <simking95@naver.com>
2022-11-07 17:48:18 +0900

    [상품 도메인 개발] - 상품 리포지토리 개발

---
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package com.sim.jpashop.repository;
+
+import com.sim.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { // Item은 JPA에 저장하기전까지 id 값이 없음(새로생성한 객체)
+            em.persist(item);
+        } else {
+            em.merge(item); // update와 유사
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
