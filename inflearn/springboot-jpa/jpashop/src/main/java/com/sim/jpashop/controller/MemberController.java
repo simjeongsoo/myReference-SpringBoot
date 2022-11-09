40f55d3c9dc78ce254d98a9ecaa50727af723ca2
simjeongsoo <simking95@naver.com>
2022-11-09 16:46:31 +0900

    [웹 계층 개발] - 회원 등록, 회원 등록 컨트롤러(에러 validation)

--- inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/controller/MemberController.java
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/controller/MemberController.java
@@ -1,10 +1,16 @@
 package com.sim.jpashop.controller;
 
+import com.sim.jpashop.domain.Address;
+import com.sim.jpashop.domain.Member;
 import com.sim.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,4 +24,22 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) { // @Valid 로 MemberForm의 어노테이션 검증
+                                                                         // @Valid 뒤에 BindingResult 파라미터가 있으면 오류를 담음
+        if (result.hasErrors()) {
+            // 에러 발생시 return
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+
+    }
 }
