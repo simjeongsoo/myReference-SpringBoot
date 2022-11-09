0ff5763ac2d57ad942b67d892915365484d43698
simjeongsoo <simking95@naver.com>
2022-11-09 21:22:22 +0900

    [웹 계층 개발] - 변경 감지 기능(dirty checking) 사용, 업데이트 메서드 동작시 파라미터가 많으면 dto로 전달

---
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/service/UpdateItemDto.java
@@ -0,0 +1,12 @@
+package com.sim.jpashop.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UpdateItemDto {
+    //--업데이트 메서드 동작시 파라미터가 많으면 dto를 생성--//
+
+    //...
+
+}
