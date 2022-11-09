7b9ac1559e2fdabd9ea52ade579373bc69c9232d
simjeongsoo <simking95@naver.com>
2022-11-09 14:37:53 +0900

    [주문 도메인 개발] - 주문 검색 기능 개발, 데이터가 없을 경우 동적 쿼리, 실무x (ver1.JPQL로 처리)

--- inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/repository/OrderRepository.java
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/repository/OrderRepository.java
@@ -26,6 +26,43 @@ public class OrderRepository {
 
     //--검색--//
     public List<Order> findAll(OrderSearch orderSearch) {
+
+        //==데이터가 없을 경우 동적 쿼리, 실무x [ver1.JPQL로 처리]==//
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+    }
+
+        /*
         // 데이터가 전부 있을때의 검색 쿼리 로직
         return em.createQuery("select o from Order o join o.member m" +
                         " where o.orderStatus = :orderstatus" +
@@ -35,8 +72,6 @@ public class OrderRepository {
 //                .setFirstResult(100) // 페이징
                 .setMaxResults(1000) // 최대 1000건
                 .getResultList();
-
+                */
 
     }
-
-}
