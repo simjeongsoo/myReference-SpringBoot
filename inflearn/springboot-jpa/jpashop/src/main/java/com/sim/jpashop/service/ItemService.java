351cf5f8ef7b661b0009a290950c567dd82f0b24
simjeongsoo <simking95@naver.com>
2022-11-09 21:21:22 +0900

    [웹 계층 개발] - 변경 감지 기능(dirty checking) 사용

--- inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/service/ItemService.java
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.sim.jpashop.service;
 
+import com.sim.jpashop.domain.item.Book;
 import com.sim.jpashop.domain.item.Item;
 import com.sim.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,22 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    /**
+     * 영속성 컨텍스트가 자동 변경
+     * */
+    @Transactional // spring의 트랜잭션에 의해 커밋 -> JPA flush() : 변경점을 찾아 업데이트
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        //--변경 감지 기능(dirty checking) 사용--//
+        Item findItem = itemRepository.findOne(itemId); // 영속상태
+
+        // setter 없이 엔티티 안에서 직접 추적할 수 있는 메서드를 만들도록 설계
+//        findItem.change(name, price, stockQuantity);
+        findItem.setName(name);                         // 값 세팅
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+//        itemRepository.save(findItem);                // 필요없음
     }
 
     public List<Item> findItems() {
