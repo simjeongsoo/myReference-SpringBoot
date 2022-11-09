d2e381f1df116e47af3f49f8b46815397515a77d
simjeongsoo <simking95@naver.com>
2022-11-09 17:26:03 +0900

    [웹 계층 개발] - 상품 등록, 상품 등록 컨트롤러

---
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/controller/ItemController.java
@@ -0,0 +1,38 @@
+package com.sim.jpashop.controller;
+
+import com.sim.jpashop.domain.item.Book;
+import com.sim.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.dom4j.rule.Mode;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new") // 상품 등록 폼 컨트롤러
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new") // 상품 등록 컨트롤러
+    public String create(BookForm form) {
+
+        // 실무에서 setter 사용x
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
