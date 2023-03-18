c3e3a7bdd424dae198ecd243db8414b15a16545e
simjeongsoo <simking95@naver.com>
2023-03-18 22:56:16 +0900

    [업데이트] - add comment

--- inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/JpashopApplication.java
+++ inflearn/springboot-jpa/jpashop/src/main/java/com/sim/jpashop/JpashopApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 	public static void main(String[] args) {
 
-		LombokT lombokT = new LombokT();
-		lombokT.setData("lombok test");
-		String data = lombokT.getData();
-		System.out.println("data = " + data);
+//		LombokT lombokT = new LombokT();
+//		lombokT.setData("lombok test");
+//		String data = lombokT.getData();
+//		System.out.println("data = " + data);
 
 		SpringApplication.run(JpashopApplication.class, args);
 	}
